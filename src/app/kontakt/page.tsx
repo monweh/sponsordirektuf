46816bf83a9371b0c1b05d78e69422f52d108438
monweh <116900228+monweh@users.noreply.github.com>
2024-12-03 14:15:34 +0100

    centered title

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
         <div className="flex flex-col justify-center items-center md:mb-32 md:h-auto">
-            <h1 className="title flex justify-center items-center mt-52 mb-2">Kontakta oss</h1>
+            <h1 className="title flex text-center justify-center items-center mt-52 mb-2">Kontakta oss</h1>
             <p className="text-center">E-post: sponsordirektuf@gmail.se</p><br />
         </div>
     );
