2d348ca96b5d8d118492be1625288d64ff719c12
monweh <116900228+monweh@users.noreply.github.com>
2025-01-17 09:12:10 +0100

    Updated contact information

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -3,6 +3,7 @@ export default function Page() {
         <div className="flex flex-col justify-center items-center md:mb-32 md:h-auto">
             <h1 className="title flex text-center justify-center items-center mt-52 mb-2">Kontakta oss</h1>
             <p className="text-center">E-post: sponsordirektuf@gmail.se</p><br />
+            <p className="text-center">Telefon: +46 73-848 31 47</p><br />
         </div>
     );
 }
